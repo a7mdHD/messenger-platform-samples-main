3c225b75b1d1e34de0c9a488c0805ad0757277e1
Ahmed Hamed <ahmed.hd1011@gmail.com>
2023-12-18 13:45:42 +0200

    edit

--- app.js
+++ app.js
@@ -45,8 +45,8 @@ app.get("/", function (_req, res) {
 // Adds support for GET requests to our webhook
 app.get("/webhook", (req, res) => {
   // Your verify token. Should be a random string.
-  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
-
+  // const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+  const VERIFY_TOKEN = "test_token";
   // Parse the query params
   let mode = req.query["hub.mode"];
   let token = req.query["hub.verify_token"];
@@ -58,7 +58,6 @@ app.get("/webhook", (req, res) => {
     if (mode === "subscribe" && token === VERIFY_TOKEN) {
       // Responds with the challenge token from the request
       console.log("WEBHOOK_VERIFIED");
-      console.log(process.env.PAGE_ACCESS_TOKEN);
       res.status(200).send(challenge);
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
